33eb3a369b1f8e8ab140b691d158df6a812a05d7
Kareem Arafa <k4ho@live.com>
2020-07-21 17:18:20 +0200

    update

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 
-const port = Deno.env.get("PORT") || 5000;
+const port = 8000;
 
 const app = new Application();
 
